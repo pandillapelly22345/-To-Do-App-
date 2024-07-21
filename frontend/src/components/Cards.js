acdb996afafb66d2e2c2ce0b708b0ec773ccd08e
pandillapelly22345 <pandillapelly22345@iiitd.ac.in>
2024-07-21 19:12:01 +0530

    basic outline for dashboard

---
+++ frontend/src/components/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Cards = (props) => {
+  return (
+    <div>
+        <p></p>
+
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const Dashboard = () => {
+
+  
   return (
-    <div>Dashboard</div>
+    <div>
+      <div>
+        <h2>Welcome</h2>
+        <h2></h2>
+      </div>
+
+      <button>Add Task +</button>
+
+      <h3>To-Do</h3>
+
+      <div>
+        {/* cards */}
+      </div>
+    </div>
   )
 }
 
